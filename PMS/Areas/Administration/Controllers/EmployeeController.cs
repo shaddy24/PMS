f6e17c00eb2a2a75b8a9e7ce6b50d08f41d0e8b7
DESKTOP-1NG2DT9\balan <shaddy.h2software@gmail.com>
2022-02-02 18:20:33 +0800

    Add project files.

---
+++ PMS/Areas/Administration/Controllers/EmployeeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PMS.Areas.Administration.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Administration/Employee
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult EmployeeInfo()
+        {
+            return View();
+        }
+
+
+        public ActionResult GetEmployeeInfolist()
+        {
+            return View();
+        }
+
+        public ActionResult EmployeeName()
+        {
+            return View();
+        }
+
+           public ActionResult GetEmployeelist()
+        {
+            return View();
+        }
+
+
+
+
+
+
+
+    }
+}
